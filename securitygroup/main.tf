a483bd8f9f6d85243bd3a127e43b49bbf7c2fe41
vasumysql <vasudba85@gmail.com>
2025-09-24 14:22:20 +0530

    Create Security Group

---
+++ securitygroup/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.5.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_security_group" "example" {
+  name        = "github-vasu-sg"
+  description = "Security Group created via GitHub Actions"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "GitHubActions-SG_Vasu"
+  }
+}
